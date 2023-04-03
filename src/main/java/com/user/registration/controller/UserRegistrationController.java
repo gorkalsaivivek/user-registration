3c29c7b48a8b2ae22a02e0081a786ec32f554c77
Vivek <saivivekgorkal@gmail.com>
2023-04-03 10:34:36 -0400

    Added user-registration as base url

--- src/main/java/com/user/registration/controller/UserRegistrationController.java
+++ src/main/java/com/user/registration/controller/UserRegistrationController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/user-registration")
 public class UserRegistrationController {
 	
 	@Autowired
